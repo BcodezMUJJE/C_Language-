62f1811d5327cd7808aa34df0d77fa0bef665cdc
BcodezMUJJE <125450966+BcodezMUJJE@users.noreply.github.com>
2024-09-24 18:13:42 +0000

    discussion with kelly

---
+++ product.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	int a = 5;
+	int b = 6;
+	int product = a*b;
+
+	printf("the product is:%d\n",  product);
+	return 0;
+}
---
+++ sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int x = 4;
+    int y = 3;
+    int sum = x+y;
+
+    printf("the sum is:%d", sum);
+    return 0;
+}
